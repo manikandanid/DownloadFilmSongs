631a9a4bf70152dacd04db43b19c7b8f21662f61
manikandanid <manikandanid@gmail.com>
2014-11-11 00:21:00 +0530

    Adding ignore files & folders

--- SourceCode/DownloadFilmSongs/Form1.cs
+++ SourceCode/DownloadFilmSongs/Form1.cs
@@ -18,6 +18,7 @@ namespace DownloadFilmSongs
     public partial class Form1 : Form
     {
         Queue<SongInfo> songQueues = new Queue<SongInfo>();
+        string outputPath = Environment.CurrentDirectory;
         public Form1()
         {
             InitializeComponent();
@@ -25,93 +26,103 @@ namespace DownloadFilmSongs
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string moviefileName = string.Empty;
+            WebClient HomePageclient = new WebClient();
+            string path = outputPath + "\\HTMLPAGES";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            HomePageclient.DownloadFileAsync(new Uri(HttpUtility.UrlDecode(this.textBox1.Text)), path + "\\HomePage.html");
+            HomePageclient.DownloadFileCompleted += HomePageclient_DownloadFileCompleted;
+        }
+
+        void HomePageclient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
             string indexfileName = string.Empty;
-            if (!System.IO.File.Exists("HomePage.html"))
-            {
-                using (WebClient client = new WebClient())
-                {
-                    client.DownloadFile(this.textBox1.Text, "HomePage.html");
-                }
-            }
-
             HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
-
-            // There are various options, set as needed
             htmlDoc.OptionFixNestedTags = true;
-
-            // filePath is a path to a file containing the html
-            htmlDoc.Load("HomePage.html");
-
-            // Use:  htmlDoc.LoadHtml(xmlString);  to load from a string (was htmlDoc.LoadXML(xmlString)
-
-            // ParseErrors is an ArrayList containing any errors from the Load statement
+            if (!System.IO.File.Exists(outputPath + "\\HTMLPAGES\\HomePage.html"))
+                return;
+            htmlDoc.Load(outputPath + "\\HTMLPAGES\\HomePage.html");
+            if (htmlDoc.DocumentNode.SelectNodes("//div[@id='sidebar1']/ul/li/div[@class='textwidget']/ul/li/a[@href]") == null)
+                return;
             foreach (HtmlNode link in htmlDoc.DocumentNode.SelectNodes("//div[@id='sidebar1']/ul/li/div[@class='textwidget']/ul/li/a[@href]"))
             {
                 if (link.InnerText.ToLower().Contains("movie index"))
                 {
                     HtmlAttribute indexattr = link.Attributes["href"];
-                    WebClient client = new WebClient();
-                    
-                    indexfileName = GetRandomName() + ".html";
-                    client.DownloadFile(indexattr.Value, indexfileName);
-                    HtmlAgilityPack.HtmlDocument indexhtmlDoc = new HtmlAgilityPack.HtmlDocument();
-
-                    // There are various options, set as needed
-                    indexhtmlDoc.OptionFixNestedTags = true;
-
-                    // filePath is a path to a file containing the html
-                    indexhtmlDoc.Load(indexfileName);
-
-                    foreach (HtmlNode movielink in indexhtmlDoc.DocumentNode.SelectNodes("//div[starts-with(@id, 'azindex-')]/div[@class='azindex']/ul/li/a[@href]"))
-                    {
-
-                        SongInfo songInfo = new SongInfo();
-                        HtmlAttribute movieattr = movielink.Attributes["href"];
-                        string filmName = "Unknown";
-                        if (movielink.ChildNodes.Count > 0)
-                        {
-                            filmName = movielink.ChildNodes[0].InnerText;
-                            songInfo.MovieName = filmName;
-                        }
-                        WebClient movieclient = new WebClient();
-                        moviefileName = GetRandomName() + ".html";
-                        client.DownloadFile(movieattr.Value, moviefileName);
-                        HtmlAgilityPack.HtmlDocument moviehtmlDoc = new HtmlAgilityPack.HtmlDocument();
-
-                        // There are various options, set as needed
-                        moviehtmlDoc.OptionFixNestedTags = true;
-
-                        // filePath is a path to a file containing the html
-                        moviehtmlDoc.Load(moviefileName);
-
-                        foreach (HtmlNode songlink in moviehtmlDoc.DocumentNode.SelectNodes("//div[@class='content']/ul/li/a[@href]"))
-                        {
-                            HtmlAttribute songAttr = songlink.Attributes["href"];
-                            string songName = HttpUtility.HtmlDecode(songlink.InnerText);
-                            string songUrl = songAttr.Value;
-                            if (songName.EndsWith(".mp3"))
-                            {
-                                if (!System.IO.File.Exists(songName))
-                                    DownloadFile(HttpUtility.UrlDecode(songUrl), filmName, songName);
-                            }
-                        }
-
-                        System.IO.File.Delete(moviefileName);
-                    }
-
-                    System.IO.File.Delete(indexfileName);
+                    indexfileName = outputPath + "\\HTMLPAGES\\" + GetRandomName() + ".html";
+                    WebClient movieindexpageclient = new WebClient();
+                    movieindexpageclient.DownloadFileAsync(new Uri(HttpUtility.UrlDecode(indexattr.Value)), indexfileName, indexfileName);
+                    movieindexpageclient.DownloadFileCompleted += movieindexpageclient_DownloadFileCompleted;
+                    //System.IO.File.Delete(indexfileName);
                 }
             }
         }
 
+        void movieindexpageclient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            HtmlAgilityPack.HtmlDocument indexhtmlDoc = new HtmlAgilityPack.HtmlDocument();
+            string movieIndexPageName = e.UserState as string;
+            string moviefileName = string.Empty;
+            // There are various options, set as needed
+            indexhtmlDoc.OptionFixNestedTags = true;
+
+            // filePath is a path to a file containing the html
+            indexhtmlDoc.Load(movieIndexPageName);
+
+            if (indexhtmlDoc.DocumentNode.SelectNodes("//div[starts-with(@id, 'azindex-')]/div[@class='azindex']/ul/li/a[@href]") == null)
+                return;
+            foreach (HtmlNode movielink in indexhtmlDoc.DocumentNode.SelectNodes("//div[starts-with(@id, 'azindex-')]/div[@class='azindex']/ul/li/a[@href]"))
+            {
+
+                SongInfo songInfo = new SongInfo();
+                HtmlAttribute movieattr = movielink.Attributes["href"];
+                string filmName = "Unknown";
+                if (movielink.ChildNodes.Count > 0)
+                {
+                    filmName = movielink.ChildNodes[0].InnerText;
+                    songInfo.MovieName = filmName;
+                }
+                WebClient movieclient = new WebClient();
+                moviefileName = outputPath + "\\HTMLPAGES\\" + GetRandomName() + ".html";
+                if (!Directory.Exists(moviefileName))
+                    Directory.CreateDirectory(moviefileName);
+                movieclient.DownloadFileAsync(new Uri(HttpUtility.UrlDecode(movieattr.Value)), moviefileName, moviefileName);
+                movieclient.DownloadFileCompleted += movieclient_DownloadFileCompleted;
+            }
+        }
+
+        void movieclient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            HtmlAgilityPack.HtmlDocument moviehtmlDoc = new HtmlAgilityPack.HtmlDocument();
+            string currMovieName = e.UserState as string;
+            moviehtmlDoc.OptionFixNestedTags = true;
+            moviehtmlDoc.Load(currMovieName);
+            if (moviehtmlDoc.DocumentNode.SelectNodes("//div[@class='content']/ul/li/a[@href]") == null)
+                return;
+
+            foreach (HtmlNode songlink in moviehtmlDoc.DocumentNode.SelectNodes("//div[@class='content']/ul/li/a[@href]"))
+            {
+                HtmlAttribute songAttr = songlink.Attributes["href"];
+                string songName = outputPath + "\\SONGS\\" + currMovieName;
+                if (!Directory.Exists(songName))
+                    Directory.CreateDirectory(songName);
+                string songUrl = songAttr.Value;
+                if (songName.EndsWith(".mp3"))
+                {
+                    if (!System.IO.File.Exists(songName))
+                        DownloadFile(HttpUtility.UrlDecode(songUrl), currMovieName, songName + "\\" + HttpUtility.HtmlDecode(songlink.InnerText));
+                }
+            }
+            //System.IO.File.Delete(moviefileName);
+        }
+
         private void DownloadFile(string songUrl, string filmName, string songName)
         {
-            var client = new WebClient();
-            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
-            List<string> obj = new List<string>(new string[] { songUrl, filmName, songName });
-            client.DownloadFileAsync(new Uri(songUrl), songName);
-
+            var songclient = new WebClient();
+            //List<string> obj = new List<string>(new string[] { songUrl, filmName, songName });
+            songclient.DownloadFileAsync(new Uri(songUrl), songName, songName);
+            songclient.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
         }
 
         void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
@@ -124,6 +135,7 @@ namespace DownloadFilmSongs
             //    tagFile.Save();
             //    System.IO.File.Move(args[2], this.textBox2.Text + "\\" + args[1] + "\\" + args[2]);
             //}
+            string songName = e.UserState as string;
             MessageBox.Show("File downloaded");
         }
 
